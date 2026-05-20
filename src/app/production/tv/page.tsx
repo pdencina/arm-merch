27c158a8e91244a92ecf51946ee0c688048dfdd6
Pablo David Encina Acevedo <Pablo.encina@klap.cl>
2026-05-19 20:47:16 -0400

    nuevos cambios 345

---
+++ src/app/production/tv/page.tsx
@@ -0,0 +1,1 @@
+export default function ProductionTVPage(){return <div className="min-h-screen bg-black text-white p-10"><h1 className="text-5xl font-black">ARM MERCH TV</h1><p className="mt-4 text-zinc-400">Pantalla operacional producción lista.</p></div>}
